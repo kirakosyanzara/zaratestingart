8ae7aa13943d548737d3bb70438d22428482cba0
kirakosyanzara <57747054+kirakosyanzara@users.noreply.github.com>
2020-02-03 11:17:46 +0400

    Add files via upload

--- testSearch.py
+++ testSearch.py
@@ -4,18 +4,32 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 
+class Search:
+    def __init__(self, id):
+        self.id = id
 
+brand_id = ("searchAll")
+
+class Input:
+    def __init__(self, key):
+        self.key = key
+
+brand_key = ("adidas")
+
+class Url:
+    def __init__(self, url):
+        self.url = url
+
+brand_url = ("https://www.6pm.com/")
 
 driver = webdriver.Chrome()
-driver.get("https://www.6pm.com/")
-input_element = driver.find_element_by_id('searchAll')
-wait = WebDriverWait(driver, "10")
-input_element.send_keys("adidas")
+driver.maximize_window()
+driver.get(brand_url)
+input_element = driver.find_element_by_id(brand_id)
+wait = WebDriverWait(driver, 10)
+input_element.send_keys(brand_key)
 input_element.submit()
-#def check_exists_by_id(searchAll):
-  #  try:
-   #     webdriver.find_element_by_id(searchAll)
-  #  except NoSuchElementException:
-   #     return False
-   # return True
+wait = WebDriverWait(driver, 10)
+driver.find_element(By.XPATH, "//*[@id='root']/div[1]/header/div[3]/div/div/div/a/span").click()
+wait = WebDriverWait(driver, 100)
 driver.quit()
